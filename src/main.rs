27aed1af0aec136c710f38b468d89e0c032cffe5
Christian Kischy Haege <kischy@gmx.de>
2021-09-26 14:54:09 +0200

    Implemented SpiralDiagonalNumers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+mod spiral_diagonal_numbers;
+use spiral_diagonal_numbers::SpiralsDiagonalNumbers;
+
 fn main() {
-    println!("Hello, world!");
+    let sdn = SpiralsDiagonalNumbers::new(5);
+    for dn in sdn {
+        println!(" {} ", dn);
+    }
 }
---
+++ src/spiral_diagonal_numbers.rs
@@ -0,0 +1,59 @@
+#[derive(Debug)]
+pub struct SpiralsDiagonalNumbers {
+    max_square_width_height: u16,
+    current_diagonal_num: u128,
+    current_square_size: u16,
+    index_of_diagonal_number_in_square: u8,
+}
+
+impl SpiralsDiagonalNumbers {
+    pub fn new(max_square_width_height: u16) -> Self {
+        Self {
+            max_square_width_height,
+            current_diagonal_num: 0,
+            current_square_size: 1,
+            index_of_diagonal_number_in_square: 0,
+        }
+    }
+
+    fn last_number_reached(&self) -> bool {
+        self.current_square_size > self.max_square_width_height
+    }
+
+    fn update_diagonal_number(&mut self) {
+        if self.current_diagonal_num != 0 {
+            if self.index_of_diagonal_number_in_square == 4 {
+                self.index_of_diagonal_number_in_square = 0;
+                self.current_square_size += 2;
+            }
+            self.index_of_diagonal_number_in_square += 1;
+            self.current_diagonal_num += self.current_square_size as u128 - 1;
+        } else {
+            self.current_diagonal_num = 1;
+            self.index_of_diagonal_number_in_square = 4;
+        }
+    }
+}
+
+impl Iterator for SpiralsDiagonalNumbers {
+    type Item = u128;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.update_diagonal_number();
+        if self.last_number_reached() == false {            
+            Some(self.current_diagonal_num)
+        } else {
+            None
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::SpiralsDiagonalNumbers;
+    #[test]
+    fn sum_of_diagonals_spiral_size_5() {
+        let sdn = SpiralsDiagonalNumbers::new(5);
+        assert_eq!(sdn.sum::<u128>(), 101);
+    }
+}
